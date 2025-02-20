b455da8e73928a02868dbdbbf318640395cb3e8e
YueJin <yuejin07@gmail.com>
2025-02-20 22:57:10 +0200

    Clean Up

--- src/js/helpers/base-helpers.js
+++ src/js/helpers/base-helpers.js
@@ -1,53 +1,52 @@
-import { MobileChecker } from './mobile-checker';
+import { MobileChecker } from "./mobile-checker";
 
 export class BaseHelpers {
 	static html = document.documentElement;
-	static header = document.querySelector('.header');
-	static firstScreen = document.querySelector('[data-observ]');
+	static header = document.querySelector(".header");
+	static firstScreen = document.querySelector("[data-observ]");
 
 	static checkWebpSupport() {
 		const testWebp = (callback) => {
 			const webP = new Image();
 
 			webP.onload = webP.onerror = () => callback(webP.height === 2);
-			webP.src =
-				'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+			webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 		};
 
 		testWebp((support) => {
-			const className = support ? 'webp' : 'no-webp';
+			const className = support ? "webp" : "no-webp";
 			BaseHelpers.html.classList.add(className);
 
-			console.log(support ? 'webp поддерживается' : 'webp не поддерживается');
+			console.log(support ? "webp поддерживается" : "webp не поддерживается");
 		});
 	}
 
 	static addTouchClass() {
 		if (MobileChecker.isAny) {
-			BaseHelpers.html.classList.add('touch');
+			BaseHelpers.html.classList.add("touch");
 		}
 	}
 
 	static addLoadedClass() {
-		window.addEventListener('load', () => {
+		window.addEventListener("load", () => {
 			setTimeout(() => {
-				BaseHelpers.html.classList.add('loaded');
+				BaseHelpers.html.classList.add("loaded");
 			}, 0);
 		});
 	}
 
 	static get getHash() {
-		return location.hash?.replace('#', '');
+		return location.hash?.replace("#", "");
 	}
 
 	static setHash(hash) {
-		hash = hash ? `#${hash}` : location.href.split('#')[0];
-		history.pushState('', '', hash);
+		hash = hash ? `#${hash}` : location.href.split("#")[0];
+		history.pushState("", "", hash);
 	}
 
 	static headerFixed() {
 		const headerStickyObserver = new IntersectionObserver(([entry]) => {
-			BaseHelpers.html.classList.toggle('header-is-sticky', !entry.isIntersecting);
+			BaseHelpers.html.classList.toggle("header-is-sticky", !entry.isIntersecting);
 		});
 
 		if (BaseHelpers.firstScreen) {
@@ -56,7 +55,7 @@ export class BaseHelpers {
 	}
 
 	static calcScrollbarWidth() {
-		const scrollbarWidth = (window.innerWidth - document.body.clientWidth) / 16 + 'rem';
-		BaseHelpers.html.style.setProperty('--bh-scrollbar-width', scrollbarWidth);
+		const scrollbarWidth = (window.innerWidth - document.body.clientWidth) / 16 + "rem";
+		BaseHelpers.html.style.setProperty("--bh-scrollbar-width", scrollbarWidth);
 	}
 }
